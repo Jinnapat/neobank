eece9ae88a693b9d03a3ec23bef859557a63daa5
Jinnapat <jaja_yana@hotmail.com>
2022-03-16 14:06:08 +0700

    Project setup

---
+++ pages/signup.js
@@ -0,0 +1,13 @@
+const SignupPage = () => {
+    return (
+        <div className="flex flex-col max-w-lg mx-auto">
+            <h1>Sign up for your business</h1>
+            <input type="text" placeholder="business name"/>
+            <input type="text" placeholder=""/>
+            <input type="text" placeholder=""/>
+            <button>Sign up</button>
+        </div>
+    )
+}
+
+export default SignupPage
