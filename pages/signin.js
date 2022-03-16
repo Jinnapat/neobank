66fc24dbd3d41d710c62c77ffe30aff8eb6c5803
Jinnapat <jaja_yana@hotmail.com>
2022-03-16 15:16:00 +0700

    Add Signin page

---
+++ pages/signin.js
@@ -0,0 +1,14 @@
+import { useState } from "react"
+
+const SigninPage = () => {
+    return (
+        <div>
+            <p>for business</p>
+            
+            <p>for supplier</p>
+            <button>connect wallet</button>
+        </div>
+    )
+}
+
+export default SigninPage
