01fd0edd2cf59532e2da0001a330d86e261f1c68
Jinnapat <jaja_yana@hotmail.com>
2022-03-18 13:36:32 +0700

    Add MainButton and ListBox components

---
+++ components/MainButton.js
@@ -0,0 +1,9 @@
+const MainButton = ({children}) => {
+    return (
+        <button className="bg-blue-600 px-5 py-2 rounded-xl text-white">
+            {children}
+        </button>
+    )
+}
+
+export default MainButton
