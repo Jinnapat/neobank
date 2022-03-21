cc3e6c6279d2ddec918a3c981449f2984f95864e
Jinnapat <jaja_yana@hotmail.com>
2022-03-21 23:24:09 +0700

    Add and update components used in verify page

--- components/MainButton.js
+++ components/MainButton.js
@@ -1,6 +1,6 @@
-const MainButton = ({children}) => {
+const MainButton = ({children, onClick}) => {
     return (
-        <button className="bg-blue-600 px-5 py-2 rounded-xl text-white">
+        <button className="bg-blue-600 px-5 py-2 rounded-xl text-white" onClick={onClick}>
             {children}
         </button>
     )
--- components/PaddedInputField.js
+++ components/PaddedInputField.js
@@ -1,15 +1,16 @@
-const PaddedInputField = ({type, placeholder, setter,value}) => {
+const PaddedInputField = ({type, placeholder, setter, value, size, other}) => {
     const updateState = (e) => {
         setter(e.target.value)
     }
 
     return (
         <input
-            className='p-2 border-2 rounded' 
+            className={'p-2 border-2 rounded' + (size ? " " + size: "")}
             type={type}
             onChange={updateState}
             placeholder={placeholder}
             value={value}
+            {...other}
         />
     )
 }
